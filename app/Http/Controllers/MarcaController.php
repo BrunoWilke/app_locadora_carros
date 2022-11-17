64a857e2da3fedb0be568d4ce6fe8b9db34c9b8c
Bruno Wilke <brunowilke153@gmail.com>
2022-11-17 11:29:44 -0300

    First commit. The project have just been initialized and configured

---
+++ app/Http/Controllers/MarcaController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Marca;
+use Illuminate\Http\Request;
+
+class MarcaController extends Controller
+{
+    
+    public function __construct(Marca $marca){
+        $this->marca = $marca;
+    }
+    
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $marcas = $this->marca->all();
+        return $marcas;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $marca = $this->marca->create($request->all());
+        return response()->json($marca,201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $marca = $this->marca->find($id);
+        if($marca === null){
+            return response()->json(['erro' => 'Recurso pesquisado nao existe'],404);
+        }
+        return response()->json($marca,200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Marca $marca)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $marca = $this->marca->find($id);
+        
+        if($marca === null){
+            return response()->json(['erro' => 'Recurso pesquisado nao existe e nao foi possível atualizá-lo'],404);
+        }
+        $marca->update($request->all());
+        return response()->json($marca,200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $marca = $this->marca->find($id);
+        
+        if($marca === null){
+            return response()->json(['erro' => 'Recurso pesquisado nao existe e nao foi possível deleta-lo'],404);
+        }
+        $marca->delete();
+        return response()->json($marca,200);
+    }
+}
