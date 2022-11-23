a40a2aa387d950e82fcef60a5d0b9d7f9f3152b6
Bruno Wilke <brunowilke153@gmail.com>
2022-11-23 10:57:47 -0300

    implemented modelo controller

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Modelo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ModeloController extends Controller
 {
+    
+    public function __construct(Modelo $modelo){
+        $this->modelo = $modelo;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +20,8 @@ class ModeloController extends Controller
      */
     public function index()
     {
-        //
+        $modelo = $this->modelo->all();
+        return $modelo;
     }
 
     /**
@@ -35,7 +42,22 @@ class ModeloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->modelo->rules());
+        
+        $imagem = $request->file('imagem');
+        $imagem_urn = $imagem->store('imagens/modelos','public');
+
+        
+        $modelo = $this->modelo->create([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs
+        ]);
+        return response()->json($modelo,201);
     }
 
     /**
@@ -44,9 +66,13 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function show(Modelo $modelo)
+    public function show($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+        if($modelo === null){
+            return response()->json(['erro' => 'Recurso pesquisado nao existe'],404);
+        }
+        return response()->json($modelo,200);
     }
 
     /**
@@ -67,9 +93,46 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, $id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+        
+        if($modelo === null){
+            return response()->json(['erro' => 'Recurso pesquisado nao existe e nao foi possível atualizá-lo'],404);
+        }
+        
+        if($request->method() === 'PATCH'){
+
+            $regrasDinamicas = array();
+
+            foreach($modelo->rules() as $input => $regra){
+                if(array_key_exists($input, $request->all())){
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($regrasDinamicas);
+
+        }else{
+            $request->validate($modelo->rules());
+        };
+        //exlui a imagem antiga da pasta public
+        if($request->file('imagem')){
+            Storage::disk('public')->delete($modelo->imagem);
+        }
+        $imagem = $request->file('imagem');
+        $imagem_urn = $imagem->store('imagens/modelos','public');
+        
+        $modelo->update([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs
+        ]);
+        return response()->json($modelo,200);
     }
 
     /**
@@ -78,8 +141,15 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Modelo $modelo)
+    public function destroy($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+        
+        if($modelo === null){
+            return response()->json(['erro' => 'Recurso pesquisado nao existe e nao foi possível deleta-lo'],404);
+        }
+        Storage::disk('public')->delete($modelo->imagem);
+        $modelo->delete();
+        return response()->json($modelo,200);
     }
 }
