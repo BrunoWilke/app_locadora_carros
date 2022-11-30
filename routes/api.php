4867666fb9777ba39a4390738455747965bd81b6
Bruno Wilke <brunowilke153@gmail.com>
2022-11-30 14:04:43 -0300

    configuring jwt

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,8 @@ Route::apiResource('carro','App\Http\Controllers\CarroController');
 Route::apiResource('marca','App\Http\Controllers\MarcaController');
 Route::apiResource('modelo','App\Http\Controllers\ModeloController');
 Route::apiResource('locacao','App\Http\Controllers\LocacaoController');
+
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+Route::post('me', 'App\Http\Controllers\AuthController@me');
