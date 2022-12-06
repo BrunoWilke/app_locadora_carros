2cb2ae6b19e5da0423991b68379806aeeb3e6816
Bruno Wilke <brunowilke153@gmail.com>
2022-12-06 11:21:47 -0300

    implemented Login.vue usin csrf_token and tokenJWT

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,18 +1,18 @@
 <template>
-        <div class="container">
+
+    <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Login (Componente Vue)</div>
-
                     <div class="card-body">
-                        <form method="POST" action="#">
-                   
+                        <form method="POST" action="" @submit.prevent="login($event)">
+                            <input type="hidden" name="_token" :value="csrf_token">
                             <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">Endereço de Email</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-right">E-mail</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" value="email" required autocomplete="email" autofocus>
+                                    <input id="email" type="email" class="form-control" name="email" value="" required autocomplete="email" autofocus v-model="email">
                                 </div>
                             </div>
 
@@ -20,7 +20,7 @@
                                 <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
+                                    <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password" v-model="password">
                                 </div>
                             </div>
 
@@ -30,7 +30,7 @@
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember">
 
                                         <label class="form-check-label" for="remember">
-                                            Mantenha-me Conectado
+                                            Mantenha-me conectado
                                         </label>
                                     </div>
                                 </div>
@@ -42,10 +42,10 @@
                                         Login
                                     </button>
 
-                                        <a class="btn btn-link" href="#">
-                                            Esqueci a Senha
-                                        </a>
-                                    
+
+                                    <a class="btn btn-link" href="#">
+                                        Esqueci a senha
+                                    </a>
                                 </div>
                             </div>
                         </form>
@@ -54,8 +54,40 @@
             </div>
         </div>
     </div>
+
 </template>
 
 <script>
-    
+    export default {
+        props: ['csrf_token'], //data (semelhante)
+        data() {
+            return {
+                email: '',
+                password: ''
+            }
+        },
+        methods: {
+            login(e) {
+
+                let url = 'http://localhost:8000/api/login'
+                let configuracao = {
+                    method: 'post',
+                    body: new URLSearchParams({
+                        'email': this.email,
+                        'password': this.password
+                    })
+                }
+
+                fetch(url, configuracao)
+                    .then(response => response.json())
+                    .then(data => {
+                        if(data.token) {
+                            document.cookie = 'token='+data.token+';SameSite=Lax'
+                        }
+                        //dar sequência no envio do form de autenticação por sessão
+                        e.target.submit()
+                    })
+            }
+        }
+    }
 </script>
