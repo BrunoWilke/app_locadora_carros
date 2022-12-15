ca4776bc4c9a5f4d7e99f9996b5bcfb73becb861
Bruno Wilke <brunowilke153@gmail.com>
2022-12-15 10:41:08 -0300

    created inputConteiner, Card and Table.vue

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -2,13 +2,42 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Marcas</div>
+                 <!-- início do card de busca -->
+                <card-component titulo="Busca de marcas">
+                    <template v-slot:conteudo>
+                        <div class="form-row">
+                            <div class="col mb-3">
+                                <input-container-component titulo="ID" id="inputId" id-help="idHelp" texto-ajuda="Opcional. Informe o ID da marca">
+                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                                </input-container-component>
+                            </div>
+                            <div class="col mb-3">
+                                <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp" texto-ajuda="Opcional. Informe o nome da marca">
+                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
+                                </input-container-component>
+                            </div>
+                        </div>
+                    </template>
 
-                    <div class="card-body">
-                        <p>Vamos manipular os registros de marcas por aqui!</p>
-                    </div>
-                </div>
+                    <template v-slot:rodape>
+                        <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
+                    </template>
+                </card-component>
+                <!-- fim do card de busca -->
+
+
+                <!-- início do card de listagem de marcas -->
+                <card-component titulo="Relação de marcas">
+                    <template v-slot:conteudo>
+                        <table-component></table-component>
+                    </template>
+
+                    <template v-slot:rodape>
+                        <button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#modalMarca">Adicionar</button>
+                    </template>
+                </card-component>
+                <!-- fim do card de listagem de marcas -->
+
             </div>
         </div>
     </div>
